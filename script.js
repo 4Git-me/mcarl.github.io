291982506d44f73d57fabbf39fd54e7dd823849a
4Git-me <115394925+4Git-me@users.noreply.github.com>
2023-03-08 15:15:10 +0800

    Initial Portfolio

---
+++ script.js
@@ -0,0 +1,9 @@
+/* Set the width of the side navigation bar to 250px */
+function openNav() {
+	document.querySelector(".sidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation bar to 0 */
+function closeNav() {
+	document.querySelector(".sidenav").style.width = "0";
+}
